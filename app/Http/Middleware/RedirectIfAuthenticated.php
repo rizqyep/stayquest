44278c0f4856e542ead718bfaa648d33be0b25c6
Rizqy EP <rizqyepr@gmail.com>
2021-07-05 06:22:10 +0700

    Feat: review on admin

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,12 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            if(Auth::user()->role=="USER"){
+                return redirect("/user/dashboard");
+            }
+            else{
+                return redirect("/admin/dashboard");
+            }
         }
 
         return $next($request);
